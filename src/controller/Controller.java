1d3dce9c76393ac12c271e15751e59487e428af5
MortenFeldtStudent <cph-mf227@cphbusiness.dk>
2020-08-07 15:13:52 +0200

    Project set up
    Project set up by MVC (Model, View, Controller) packages.

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,14 +1,11 @@
-package secondcase_moviecase;
+package controller;
 
 /**
  *
  * @author Morten
  */
-public class SecondCase_MovieCase {
+public class Controller {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
     }
---
+++ src/view/Console.java
@@ -0,0 +1,12 @@
+package view;
+
+/**
+ * The purpose of Console: !!!TYPE PURPOSE OF Console HERE!!!
+ * @author Morten
+ * @version 1.0
+ * @since 07-08-2020
+ */
+
+public class Console {
+
+}
