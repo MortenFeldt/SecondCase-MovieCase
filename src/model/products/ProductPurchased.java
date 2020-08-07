28dc1d7cd73677cddfd66624846cfb80ec5d09ef
MortenFeldtStudent <cph-mf227@cphbusiness.dk>
2020-08-07 18:56:04 +0200

    Product purchased inheritance
    Impelmented code for purchaed product inheritance used when count most popular product.

---
+++ src/model/products/ProductPurchased.java
@@ -0,0 +1,36 @@
+package model.products;
+
+import java.util.List;
+
+/**
+ * Class to hold count of Product purchased. This class extends from Product class.
+ * @author Morten Feldt
+ */
+
+public class ProductPurchased extends Product{
+    
+    private int purchasedcount;
+
+    public ProductPurchased(String name, int rating, double price, int purchasedcount) {
+        super(name, rating, price);
+        this.purchasedcount = purchasedcount;
+    }
+
+    public ProductPurchased(String name, List<String> genre, int rating, double price, int purchasedcount) {
+        super(name, genre, rating, price);
+        this.purchasedcount = purchasedcount;
+    }
+
+    public int getPurchasedcount() {
+        return purchasedcount;
+    }
+
+    public void setPurchasedcount(int purchasedcount) {
+        this.purchasedcount = purchasedcount;
+    }
+    
+    public void countUpPurchasedCount(){
+        this.purchasedcount += 1;
+    }
+
+}
