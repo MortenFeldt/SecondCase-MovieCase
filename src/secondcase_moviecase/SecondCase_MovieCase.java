23c41fac1ec0edc0fed70c6408c45e113b910cb3
MortenFeldtStudent <cph-mf227@cphbusiness.dk>
2020-08-07 15:00:41 +0200

    First commit
    First commit with new readme file and project.

---
+++ src/secondcase_moviecase/SecondCase_MovieCase.java
@@ -0,0 +1,16 @@
+package secondcase_moviecase;
+
+/**
+ *
+ * @author Morten
+ */
+public class SecondCase_MovieCase {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
