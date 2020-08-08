7ae28601490862b3a7245556bce1dc21dbd3eb82
MortenFeldtStudent <cph-mf227@cphbusiness.dk>
2020-08-08 11:04:29 +0200

    ProductPurchased test code
    Implemented test code for ProductPurchased object.

--- test/model/products/ProductPurchasedTest.java
+++ test/model/products/ProductPurchasedTest.java
@@ -1,7 +1,9 @@
 package model.products;
 
+import java.util.ArrayList;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Before;
 
 /**
  * Test class to test functionality about ProductPurchased class object
@@ -9,7 +11,48 @@ import static org.junit.Assert.*;
  */
 public class ProductPurchasedTest {
     
-    public ProductPurchasedTest() {
+    private String productName = "";
+    private int productRating = 0;
+    private double productPrice = 0.0;
+    private ArrayList<String> productGenre = new ArrayList();
+    private int productPurchased;
+    
+    @Before
+    public void init(){
+        productName = "Test Product 1";
+        productRating = 1;
+        productPrice = 134.95;
+        productGenre.add("Action");
+        productGenre.add("Thrillre");
+        productGenre.add("Comedy");  
+        productPurchased = 23;
+    }
+    
+    @Test
+    public void testGetPurchasedCount(){
+        ProductPurchased productPurchasedTest = new ProductPurchased(productName, productGenre, productRating, productPrice, productPurchased);
+        int expected = 23;
+        int actual = productPurchasedTest.getPurchasedcount();
+        assertEquals(expected, actual);
+    }
+    
+    @Test
+    public void testSetPurchasedCount(){
+        ProductPurchased productPurchasedTest = new ProductPurchased(productName, productGenre, productRating, productPrice, productPurchased);
+        int newPurchasedCount = 50;
+        productPurchasedTest.setPurchasedcount(newPurchasedCount);
+        int expected = 50;
+        int actual = productPurchasedTest.getPurchasedcount();
+        assertEquals(expected, actual);
+    }
+    
+    @Test
+    public void testCountUpPurchased(){
+        ProductPurchased productPurchasedTest = new ProductPurchased(productName, productGenre, productRating, productPrice, productPurchased);
+        productPurchasedTest.countUpPurchasedCount();
+        int expected = 24;
+        int actual = productPurchasedTest.getPurchasedcount();
+        assertEquals(expected, actual);
     }
     
 }
