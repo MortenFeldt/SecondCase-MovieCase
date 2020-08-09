679c42476e3181f3d6e14db323cc43dec026097c
MortenFeldt <mortenguy@hotmail.com>
2020-08-09 11:50:01 +0200

    Controller start up code
    Implemented code for start up for controller.

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,13 +1,37 @@
 package controller;
 
+import java.io.FileNotFoundException;
+import model.File.FileFacadeImpl;
+import model.interfaces.FileFacade;
+import model.products.Product;
+import model.products.Products;
+import model.users.User;
+import model.users.Users;
+
 /**
  *
  * @author Morten Feldt
  */
 public class Controller {
 
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws FileNotFoundException {
+        FileFacadeImpl fileFacadeImpl = new FileFacadeImpl();
+        
+        Products products = fileFacadeImpl.getProductDataFromFile("Products.txt");
+        
+        for (Product product : products.getProducts().values()) {
+            System.out.println("*************************************");
+            System.out.println(product.getName());
+            System.out.println("*************************************");
+        }
+        
+        Users users = fileFacadeImpl.getUserDataFromFile("Users.txt", products);
+        
+        for (User user : users.getUsers().values()) {
+            System.out.println("*************************************");
+            System.out.println(user.getViewed().size());
+            System.out.println("*************************************");
+        }
     }
     
 }
